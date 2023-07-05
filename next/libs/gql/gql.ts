11519267da9dde0736d618e70849f794a4a7d57e
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-05 21:54:56 +0900

    client side codegen

--- next/libs/gql/gql.ts
+++ next/libs/gql/gql.ts
@@ -15,6 +15,8 @@ import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/
 const documents = {
   "\n  fragment MarkdownFragment on Markdown {\n    contents\n  }\n":
     types.MarkdownFragmentFragmentDoc,
+  "\n  query IndexPage {\n    markdown {\n      ...MarkdownFragment\n    }\n  }\n":
+    types.IndexPageDocument,
 };
 
 /**
@@ -35,8 +37,14 @@ export function graphql(source: string): unknown;
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
 export function graphql(
-  source: "\n  fragment MarkdownFragment on Markdown {\n    contents\n  }\n"
+  source: "\n  fragment MarkdownFragment on Markdown {\n    contents\n  }\n",
 ): (typeof documents)["\n  fragment MarkdownFragment on Markdown {\n    contents\n  }\n"];
+/**
+ * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
+ */
+export function graphql(
+  source: "\n  query IndexPage {\n    markdown {\n      ...MarkdownFragment\n    }\n  }\n",
+): (typeof documents)["\n  query IndexPage {\n    markdown {\n      ...MarkdownFragment\n    }\n  }\n"];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
--- next/libs/gql/graphql.ts
+++ next/libs/gql/graphql.ts
@@ -1,93 +1,70 @@
 /* eslint-disable */
-import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
-export type MakeEmpty<
-  T extends { [key: string]: unknown },
-  K extends keyof T
-> = { [_ in K]?: never };
-export type Incremental<T> =
-  | T
-  | {
-      [P in keyof T]?: P extends " $fragmentName" | "__typename" ? T[P] : never;
-    };
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: { input: string; output: string };
-  String: { input: string; output: string };
-  Boolean: { input: boolean; output: boolean };
-  Int: { input: number; output: number };
-  Float: { input: number; output: number };
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
 };
 
 export type Markdown = {
-  __typename: "Markdown";
-  contents?: Maybe<Scalars["String"]["output"]>;
+  __typename: 'Markdown';
+  contents?: Maybe<Scalars['String']['output']>;
 };
 
 export type Mutation = {
-  __typename: "Mutation";
+  __typename: 'Mutation';
   createTodo: Todo;
 };
+
 
 export type MutationCreateTodoArgs = {
   input: NewTodo;
 };
 
 export type NewTodo = {
-  text: Scalars["String"]["input"];
-  userId: Scalars["String"]["input"];
+  text: Scalars['String']['input'];
+  userId: Scalars['String']['input'];
 };
 
 export type Query = {
-  __typename: "Query";
+  __typename: 'Query';
+  markdown?: Maybe<Markdown>;
   todos: Array<Todo>;
 };
 
 export type Todo = {
-  __typename: "Todo";
-  done: Scalars["Boolean"]["output"];
-  id: Scalars["ID"]["output"];
-  text: Scalars["String"]["output"];
+  __typename: 'Todo';
+  done: Scalars['Boolean']['output'];
+  id: Scalars['ID']['output'];
+  text: Scalars['String']['output'];
   user: User;
 };
 
 export type User = {
-  __typename: "User";
-  id: Scalars["ID"]["output"];
-  name: Scalars["String"]["output"];
+  __typename: 'User';
+  id: Scalars['ID']['output'];
+  name: Scalars['String']['output'];
 };
 
-export type MarkdownFragmentFragment = {
-  __typename: "Markdown";
-  contents?: string | null;
-} & { " $fragmentName"?: "MarkdownFragmentFragment" };
+export type MarkdownFragmentFragment = { __typename: 'Markdown', contents?: string | null } & { ' $fragmentName'?: 'MarkdownFragmentFragment' };
 
-export const MarkdownFragmentFragmentDoc = {
-  kind: "Document",
-  definitions: [
-    {
-      kind: "FragmentDefinition",
-      name: { kind: "Name", value: "MarkdownFragment" },
-      typeCondition: {
-        kind: "NamedType",
-        name: { kind: "Name", value: "Markdown" },
-      },
-      selectionSet: {
-        kind: "SelectionSet",
-        selections: [
-          { kind: "Field", name: { kind: "Name", value: "contents" } },
-        ],
-      },
-    },
-  ],
-} as unknown as DocumentNode<MarkdownFragmentFragment, unknown>;
+export type IndexPageQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type IndexPageQuery = { __typename: 'Query', markdown?: (
+    { __typename: 'Markdown' }
+    & { ' $fragmentRefs'?: { 'MarkdownFragmentFragment': MarkdownFragmentFragment } }
+  ) | null };
+
+export const MarkdownFragmentFragmentDoc = {"kind":"Document","definitions":[{"kind":"FragmentDefinition","name":{"kind":"Name","value":"MarkdownFragment"},"typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"Markdown"}},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"contents"}}]}}]} as unknown as DocumentNode<MarkdownFragmentFragment, unknown>;
+export const IndexPageDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"IndexPage"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"markdown"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"FragmentSpread","name":{"kind":"Name","value":"MarkdownFragment"}}]}}]}},{"kind":"FragmentDefinition","name":{"kind":"Name","value":"MarkdownFragment"},"typeCondition":{"kind":"NamedType","name":{"kind":"Name","value":"Markdown"}},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"contents"}}]}}]} as unknown as DocumentNode<IndexPageQuery, IndexPageQueryVariables>;
