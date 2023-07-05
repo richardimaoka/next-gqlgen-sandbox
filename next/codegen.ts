ff9051d5969e37e07a3022fb4b58a62b0df64acc
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-05 20:47:39 +0900

    npx graphql-code-generator init

---
+++ next/codegen.ts
@@ -0,0 +1,26 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "../gqlgen/graph/schema.graphqls",
+  documents: ["pages/**/*.tsx", "components/**/*.tsx"],
+  generates: {
+    "libs/gql/": {
+      preset: "client",
+      plugins: [],
+      config: {
+        // avoidOptionals: true,
+        nonOptionalTypename: true, //__typename should always exist
+        enumsAsTypes: true,
+      },
+    },
+  },
+  watch: [
+    "pages/**/*.tsx",
+    "components/**/*.tsx",
+    "../gqlgen/graphq/schema.gql",
+  ],
+  hooks: { afterOneFileWrite: ["npx prettier --write"] },
+};
+
+export default config;
