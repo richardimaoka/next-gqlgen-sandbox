6b3e7a0b19b49bec04bc2a63c6a9d789adf29490
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-07 08:38:08 +0900

    ExtractTypeName

---
+++ gqlgen/graph/json_util.go
@@ -0,0 +1,26 @@
+package graph
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+)
+
+func ExtractTypeName(jsonBytes []byte, fromField string) (string, error) {
+	var unmarshaled map[string]interface{}
+	if err := json.Unmarshal(jsonBytes, &unmarshaled); err != nil {
+		return "", err
+	}
+
+	typeNameRaw, ok := unmarshaled[fromField]
+	if !ok {
+		return "", fmt.Errorf("\"%s\" does not exist in JSON", fromField)
+	}
+
+	typeName, ok := typeNameRaw.(string)
+	if !ok {
+		return "", fmt.Errorf("\"%s\" is not a string, but found in type = %v", fromField, reflect.TypeOf(typeNameRaw))
+	}
+
+	return typeName, nil
+}
