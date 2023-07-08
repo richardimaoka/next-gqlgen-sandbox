6f5dce2a69df769337108a8fedd3b7fe526be53b
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-08 23:38:24 +0900

    steps

--- gqlgen/processing/read/all.go
+++ gqlgen/processing/read/all.go
@@ -3,23 +3,29 @@ package read
 import "fmt"
 
 type All struct {
+	Steps                  Steps
 	ImageDecriptionColumns ImageDecriptionColumns
 	BackgroundImageColumns BackgroundImageColumns
 }
 
-func InitialRead(dirName string) error {
+func InitialRead(dirName string) (*All, error) {
 	a := All{}
 	var err error
 
+	a.Steps, err = ReadSteps(dirName + "/steps.json")
+	if err != nil {
+		return nil, fmt.Errorf("InitialRead failed, %s", err)
+	}
+
 	a.BackgroundImageColumns, err = ReadBackgroundImageColumns(dirName + "/bg_columns.json")
 	if err != nil {
-		return fmt.Errorf("InitialRead failed, %s", err)
+		return nil, fmt.Errorf("InitialRead failed, %s", err)
 	}
 
 	a.ImageDecriptionColumns, err = ReadImageDecriptionColumns(dirName + "/img_columns.json")
 	if err != nil {
-		return fmt.Errorf("InitialRead failed, %s", err)
+		return nil, fmt.Errorf("InitialRead failed, %s", err)
 	}
 
-	return nil
+	return &a, nil
 }
