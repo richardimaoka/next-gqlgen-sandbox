b84b82df9a3a7c9af957309943a93805b0c58aac
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-07-09 17:53:38 +0900

    flag

--- gqlgen/processing/convert/effects_test.go
+++ gqlgen/processing/convert/effects_test.go
@@ -3,12 +3,15 @@ package convert_test
 import (
 	"testing"
 
+	"github.com/richardimaoka/next-gqlgen-sandbox/gqlgen/internal"
 	"github.com/richardimaoka/next-gqlgen-sandbox/gqlgen/processing/convert"
 )
 
 func TestReadEffects(t *testing.T) {
-	_, err := convert.ReadEffects("testdata")
+	effects, err := convert.ReadEffects("testdata")
 	if err != nil {
 		t.Fatalf("ReadEffects failed to read file, %s", err)
 	}
+
+	internal.CompareWitGoldenFile(t, *updateFlag, "testdata/effects_golden.json", effects)
 }
