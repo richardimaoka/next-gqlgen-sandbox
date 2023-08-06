8192958f474da7593b29461c73d1ae5f58ed9bea
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-06 18:43:11 +0900

    adjust column height

--- next/app/components/column/VisibleColumn.tsx
+++ next/app/components/column/VisibleColumn.tsx
@@ -1,9 +1,11 @@
+"use client";
 import { FragmentType, graphql, useFragment } from "@/libs/gql";
 import { ColumnHeader } from "./ColumnHeader";
 
 import styles from "./style.module.css";
 import { ColumnWrapperComponent } from "./ColumnWrapperComponent";
 import { nonNullArray } from "@/libs/nonNullArray";
+import { useState } from "react";
 
 const fragmentDefinition = graphql(`
   fragment VisibleColumn_Fragment on Page {
@@ -17,23 +19,21 @@ const fragmentDefinition = graphql(`
 
 interface VisibleColumnProps {
   fragment: FragmentType<typeof fragmentDefinition>;
-  selectColumn: string;
 }
 
 export const VisibleColumn = (props: VisibleColumnProps) => {
   const fragment = useFragment(fragmentDefinition, props.fragment);
+  const [selectColumn, setSelectColumn] = useState<string>("Terminal");
 
   if (!fragment?.columns) {
     return <div></div>;
   }
   const columns = nonNullArray(fragment.columns);
-  const visibleColumn = columns.find(
-    (column) => column.name === props.selectColumn
-  );
+  const visibleColumn = columns.find((column) => column.name === selectColumn);
 
   return (
     <div className={styles.visiblecolumn}>
-      <ColumnHeader fragment={fragment} selectColumn={props.selectColumn} />
+      <ColumnHeader fragment={fragment} selectColumn={selectColumn} />
       <div className={styles.wrapper}>
         {/* above <div> + .wrapper style is necessary to control the height of visible column = 100svh */}
         {visibleColumn && <ColumnWrapperComponent fragment={visibleColumn} />}
